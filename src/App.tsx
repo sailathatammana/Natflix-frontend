5649fb1dbfe74bbee468b37cffa4ae0b144a9150
Eduardo <el_alienx@hotmail.com>
2022-10-01 14:55:50 +0200

    added button style and fixed the paragraph sizes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import "./styles/style.css";
 export default function App() {
   return (
     <div className="App">
+      {/* Typography */}
       <h1>Heading 1</h1>
       <h2>Heading 2</h2>
       <h3>Heading 3</h3>
@@ -21,6 +22,11 @@ export default function App() {
         sequi voluptates magnam laborum facere numquam quisquam, tempora
         recusandae molestiae!
       </p>
+
+      {/* Buttons */}
+      <button className="button">Click me!</button>
+
+      {/* Cards */}
       <CardBasic id={42} imageURL="https://picsum.photos/300/200" />
       <CardTop10 id={69} number={1} imageURL="https://picsum.photos/200/300" />
       <CardTop10 id={70} number={5} imageURL="https://picsum.photos/200/300" />
