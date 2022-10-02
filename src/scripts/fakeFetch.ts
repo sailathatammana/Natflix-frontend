08f2162999a669ab8b19b1b518727f186a44536a
Eduardo <el_alienx@hotmail.com>
2022-10-02 20:33:57 +0200

    organizing data

--- src/scripts/fakeFetch.ts
+++ src/scripts/fakeFetch.ts
@@ -3,34 +3,84 @@
  * This method is fake replacement for the real fetch method in JavaScript.
  */
 
-import Content from "fake-data/content.json";
+// Fake data
+import Content from "./fake-data/content.json";
+import SingleMovie from "./fake-data/singleMovie.json";
+import SingleSerie from "./fake-data/singleSerie.json";
+import SingleDocumentary from "./fake-data/singleDocumentary.json";
+
+// Project files
+import iContent from "interfaces/iContent";
 
 interface iResponse {
   status: string;
-  data: Array<any>;
+  data: Array<any> | Object;
 }
 
 export default async function fakeFetch(): Promise<iResponse> {
   const chanceToSucced = Math.floor(Math.random() * 10);
-  let status: string = "pending";
-  let data: Array<any> = [];
+  const result = { data: {}, status: "" };
 
   await delay(1000);
 
   switch (chanceToSucced) {
     case 0:
-      status = "error";
+      result.status = "error";
       break;
     case 1:
-      data = [];
-      status = "ok"; // if worked but there is not data
+      result.data = [];
+      result.status = "ok"; // if worked but there is not data
       break;
     default:
-      data = Content;
-      status = "ok";
+      result.data = content();
+      result.status = "ok";
   }
 
-  return { status: status, data: data };
+  return result;
+}
+
+function getData(endPoint: string, id: number = 0): Array<any> | any {
+  let result: Array<any> = [];
+
+  switch (endPoint) {
+    case "content":
+      result = content();
+      break;
+    case "contentDetails":
+      result = contentDetails(id);
+      break;
+    default:
+      throw new Error("invalid endpoint");
+  }
+
+  return result;
+}
+
+function content(): Array<iContent> {
+  const result = Content;
+
+  return result;
+}
+
+function contentDetails(id: number): any {
+  const content = Content.filter((item) => item.id === id)[0];
+  let result;
+
+  switch (content.type_id) {
+    case 1:
+      result = SingleSerie;
+      break;
+    case 2:
+      result = SingleMovie;
+      break;
+    case 3:
+      result = SingleDocumentary;
+      break;
+    default:
+      result = {};
+  }
+
+  return result;
 }
 
 function delay(miliseconds: number) {
