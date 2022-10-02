641a370b018ec3de4f84e86b5d87088e3389b350
Eduardo <el_alienx@hotmail.com>
2022-10-02 19:08:24 +0200

    improving the fake fetch

--- src/scripts/fakeFetch.ts
+++ src/scripts/fakeFetch.ts
@@ -3,7 +3,7 @@
  * This method is fake replacement for the real fetch method in JavaScript.
  */
 
-import FakeContent from "fake-data/fake-content.json";
+import Content from "fake-data/content.json";
 
 interface iResponse {
   status: string;
@@ -11,20 +11,28 @@ interface iResponse {
 }
 
 export default async function fakeFetch(): Promise<iResponse> {
-  const chanceToSucced = Math.floor(Math.random() * 100);
+  const chanceToSucced = Math.floor(Math.random() * 10);
   let status: string = "pending";
   let data: Array<any> = [];
 
   await delay(1000);
 
-  if (chanceToSucced > 0) {
-    data = FakeContent;
-    status = "ok";
-  } else {
-    status = "error";
+  switch (chanceToSucced) {
+    case 0:
+      status = "error";
+      break;
+    case 1:
+      data = [];
+      status = "ok"; // if worked but there is not data
+      break;
+    default:
+      data = Content;
+      status = "ok";
   }
 
   return { status: status, data: data };
 }
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+function delay(miliseconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, miliseconds));
+}
