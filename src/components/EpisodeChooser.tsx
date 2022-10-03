6bc403d9cb53c48ab7574f8284b1549bbdd0a2f1
Eduardo Alvarez <el_alienx@hotmail.com>
2022-10-03 13:56:45 +0200

    moar code!

--- src/components/EpisodeChooser.tsx
+++ src/components/EpisodeChooser.tsx
@@ -1,3 +1,6 @@
+// Node modules
+import { useState } from "react";
+
 // Project files
 import ItemEpisodes from "components/ItemEpisode";
 import iDetailsSeries from "interfaces/iDetailsSeries";
@@ -8,12 +11,15 @@ interface iProps {
 }
 
 export default function EpisodeChooser({ episodes, onClick }: iProps) {
+  // Local state
+  const [season, setSeason] = useState(1);
+
   // Properties
   const numberOfSeasons: number = 0;
-  const selectedSeason = 1;
+  const selectedEpisodes = episodes.filter((item) => item.season === season);
 
   // Components
-  const Episodes = episodes.map((item) => (
+  const Episodes = selectedEpisodes.map((item) => (
     <ItemEpisodes key={item.id} item={item} onClick={onClick} />
   ));
 
