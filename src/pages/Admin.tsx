6f73b117904f02b76fcb28d78b78c5656eaf18cc
Eduardo Alvarez <el_alienx@hotmail.com>
2022-10-06 23:50:16 +0200

    added css

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,17 +1,30 @@
 // Node modules
 import { Link } from "react-router-dom";
 
-export default function Admin() {
-  // Reference
-  // path="/admin/:/:code"
+// Project files
+import ImageMovies from "assets/images/admin-movies.jpg";
+import ImageDocumentaries from "assets/images/admin-documentaries.jpg";
+import ImageSeries from "assets/images/admin-series.jpg";
 
+export default function Admin() {
   return (
     <div id="admin">
-      <h1>Admin panel</h1>
-
-      <Link to="/admin/content/movies">Movies</Link>
-      <Link to="/admin/content/documentaries">Documentaries</Link>
-      <Link to="/admin/content/series">TV Series</Link>
+      <h1>Admin main menu</h1>
+      <p>Please choose one of the following options to start adding content:</p>
+      <div className="items">
+        <Link className="item" to="/admin/content/movies">
+          <img src={ImageMovies} />
+          <h3>Movies</h3>
+        </Link>
+        <Link className="item" to="/admin/content/documentaries">
+          <img src={ImageDocumentaries} />
+          <h3>Documentaries</h3>
+        </Link>
+        <Link className="item" to="/admin/content/series">
+          <img src={ImageSeries} />
+          <h3>TV Series</h3>
+        </Link>
+      </div>
     </div>
   );
 }
