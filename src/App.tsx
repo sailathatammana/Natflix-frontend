39fbfdfbd2c3b93f4d7689ebec106a2b90c245a3
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-10-21 18:58:52 +0200

    added auth user

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // Node modules
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 // Project files
 import Modal from "components/Modal";
@@ -7,29 +7,22 @@ import eUserType from "interfaces/eUserType";
 import AdminRoutes from "routes/AdminRoutes";
 import CustomerRoutes from "routes/CustomerRoutes";
 import UnloggedRoutes from "routes/UnlogedRoutes";
-import { ModalProvider } from "state/ModalContext";
-import { UserProvider, useUser } from "state/UserContext";
+import { useUser } from "state/UserContext";
 import "styles/style.css";
 
 export default function App() {
   // Global state
   const { user } = useUser();
 
-  console.log("user", user);
-
   return (
     <div className="App">
-      <UserProvider>
-        <ModalProvider>
-          <BrowserRouter>
-            {user === null && <UnloggedRoutes />}
-            {user?.type === eUserType.ADMIN && <AdminRoutes />}
-            {user?.type === eUserType.CUSTOMER && <CustomerRoutes />}
-            {/* To handle the modal/popups of the website */}
-            <Modal />
-          </BrowserRouter>
-        </ModalProvider>
-      </UserProvider>
+      <BrowserRouter>
+        {user === null && <UnloggedRoutes />}
+        {user?.type === eUserType.ADMIN && <AdminRoutes />}
+        {user?.type === eUserType.CUSTOMER && <CustomerRoutes />}
+        {/* To handle the modal/popups of the website */}
+        <Modal />
+      </BrowserRouter>
     </div>
   );
 }
